24a385af81485cebede77af620bc5b7c0fd92e8b
Simon <simon.goransson@monator.com>
2022-02-24 13:48:58 +0100

    Added Hello World ! text

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
         {message && <p>{message}</p>}
       </header>
